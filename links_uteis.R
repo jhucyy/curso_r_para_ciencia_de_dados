4313c895ce2df6ff34d6384370c63c47b3d7858e
Juciane <jhucyy.pereira@gmail.com>
2022-05-21 18:04:00 -0300

    links para o curso R para ciência de dados da Curso-R

---
+++ links_uteis.R
@@ -0,0 +1,26 @@
+
+# Links para as páginas do curso R para ciência de dados -------------------
+
+
+# Visualização de dados ---------------------------------------------------
+# https://curso-r.github.io/main-r4ds-1/slides/05-ggplot2.html#6
+
+
+# manipulação de dados ----------------------------------------------------
+# https://curso-r.github.io/main-r4ds-1/slides/04-manipulacao.html#1
+
+
+# Importação dos dados - trabalhando com diretórios -----------------------
+# https://curso-r.github.io/main-r4ds-1/slides/03-importacao.html#1
+
+
+# Livro curso R - R para a ciência de dados -------------------------------
+
+# https://livro.curso-r.com/index.html
+
+
+# Configurando o git e o github no r studio -------------------------------
+# https://beatrizmilz.github.io/RLadies-Git-RStudio-2019/#1
+
+
+
